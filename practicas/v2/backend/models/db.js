a48c4852f6f074a22d26a6e8a2e11cfd71b83be7
pabloAcer <pablo.c1791@gmail.com>
2022-02-19 17:49:49 +0100

    as

--- practicas/v2/backend/models/db.js
+++ practicas/v2/backend/models/db.js
@@ -4,10 +4,14 @@ require('dotenv').config()
 
 var pool = msql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // pass: process.env.DB_PASS,
+    // database: process.env.DB_NAME
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'bb0857b524d028',
+    pass: 'a646f1c7',
+    database: 'heroku_29409a6ce3bfee7' 
 });
 
 pool.query = util.promisify(pool.query)
